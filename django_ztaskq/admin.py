c7bd2da2831bc194ce7a82dba08c2d44535b7bb4
luminous <the.luminous@riseup.now>
2013-01-21 21:07:29 +0000

    add ModelAdmin for django_ztaskq.Task

---
+++ django_ztaskq/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from zstatusapi.django_ztaskq.models import Task
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('taskid', 'function_name', 'args', 'kwargs', 'return_value',
+                    'error', 'queued', 'started', 'finished', 'status',)
+    list_filter = ('function_name', 'return_value', 'status',)
+    search_fields = ('taskid', 'function_name','args', 'kwargs', 'return_value',
+                    'error', 'queued', 'started', 'finished', 'status',)
+    readonly_fields = ('taskid', 'function_name', 'args', 'kwargs',
+                       'return_value','error', 'queued', 'started', 'finished',
+                       'status',)
+    ordering = ('queued',)
+    #fields = ('name', 'checksum', 'active', 'description', 'src')
+
+
+admin.site.register(Task, TaskAdmin)
