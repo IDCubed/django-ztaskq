b3e70f3e2a06a416d8460c7ee2afedd03989fecd
luminous <the.luminous@riseup.now>
2013-03-02 15:17:03 +0000

    commentary and whitespace in settings.py for readability

--- django_ztaskq/conf/settings.py
+++ django_ztaskq/conf/settings.py
@@ -1,11 +1,25 @@
 from django.conf import settings
 
+# ZMQ Socket zTaskq master
 ZTASKD_URL = getattr(settings, 'ZTASKD_URL', 'tcp://127.0.0.1:5555')
+
+# ZMQ socket for worker 
 ZTASK_WORKER_URL = getattr(settings, 'ZTASK_WORKER_URL', 'tcp://127.0.0.1:5556')
+
+# XXX - research to doc
 ZTASK_INTERNAL_QUEUE_URL = getattr(settings, 'ZTASK_INTERNAL_QUEUE_URL',
                                    'inproc://django_ztask_internal_queue')
+# XXX - research to doc
 ZTASKD_ON_LOAD = getattr(settings, 'ZTASKD_ON_LOAD', ())
+
+# default log verbosity
 ZTASKD_LOG_LEVEL = getattr(settings, 'ZTASKD_LOG_LEVEL', 'info')
+
+# XXX - research to doc
 ZTASKD_LOG_PATH = getattr(settings, 'ZTASKD_LOG_PATH', None)
+
+# max filesize per log file (before rollover?)
 ZTASKD_LOG_MAXBYTES = getattr(settings, 'ZTASKD_LOG_MAXBYTES', 36864)
+
+# how many log files to let rollover
 ZTASKD_LOG_BACKUP = getattr(settings, 'ZTASKD_LOG_BACKUP', 4)
